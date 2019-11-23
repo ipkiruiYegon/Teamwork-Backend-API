37b9506bd323427f7d59e6002b0e881dd7b39aad
Yegon Kipkirui Geoffrey <ipkiruig83@gmail.com>
2019-11-23 17:02:51 +0300

    Create file structure for our API server
    Update package.json
    Create eslintrc
    Add config file for production, test and development
    Create app for our server
    create routes

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const routes = require('../src/app_routes/index');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(routes);
+
+// error handler for routes not found
+app.get('*', (req, res) => {
+  res.status(404);
+  res.json({
+    status: 'error',
+    error: 'Route not found'
+  });
+});
+
+// error handler for errors
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json({
+    status: 'error',
+    error: 'An internal error occurred while processing your request'
+  });
+});
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import config from 'config';
+
+const debug = require('debug')('server:debug');
+
+const app = require('./app');
+
+const listen = app.listen(config.get('port'), () => {
+  debug(
+    `server is running on port ${config.get('port')} and in ${config.get(
+      'name'
+    )} mode`
+  );
+  console.log(
+    `server is running on port ${config.get('port')} and in ${config.get(
+      'name'
+    )} mode`
+  );
+});
+
+module.exports = app;
+module.exports.port = listen.address().port;
